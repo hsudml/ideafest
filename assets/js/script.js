cf925120051393bb253a5765b4a6a318401010b5
Tim Miller <twm2@humboldt.edu>
2019-05-30 11:35:36 -0700

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+function addAnchorLink(){
+  $('h3').each(function(i){
+    var sectionId = "section";  
+    sectionId = sectionId+i;
+    $(this).attr('id',sectionId);
+    $(this).wrap('<a href="#'+sectionId+'"></a>');
+    $(this).prepend('<ion-icon name="ios-link"></ion-icon>');
+  });
+  $('h3').hover(function(){
+    $(this).find('ion-icon').toggle(); 
+  });
+}
+addAnchorLink();
+
+});
